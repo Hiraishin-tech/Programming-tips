698ea0e8ed55e934116571f527a5c13e06795fc4
Hiraishin-tech <phamtoma@seznam.cz>
2024-08-17 19:41:46 +0200

    Update db-connect.php
    adding the function getenv in db connection.

--- assets/db-connect.php
+++ assets/db-connect.php
@@ -1,9 +1,9 @@
 <?php
 
 $db = new PDO(
-    "mysql:host=localhost;dbname=programming_tips;charset=utf8",
-    "root",
-    "Harry123.",
+    "mysql:host=".getenv("DB_HOST").";dbname=".getenv("DB_NAME").";charset=utf8",
+    getenv("DB_USER"),
+    getenv("DB_PASSWORD"),
     array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     ),
